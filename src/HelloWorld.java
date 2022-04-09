c913939c1f65829bb217ce2cbba2ef5b4f4ac740
seyda <iclalseyda@gmail.com>
2022-04-09 16:50:53 +0200

    new

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
 
     // Doing some work here
-b vb
+
 // doing awesome work 2!
 
     // doing awesome work3
     // Awesome work 3 i s done and ready t
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
